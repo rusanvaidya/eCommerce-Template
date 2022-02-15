d10b63e9222622d686c8013feed11b1d80ac80bb
Rusan Vaidya <60218372+rusanvaidya@users.noreply.github.com>
2022-02-15 18:26:47 +0545

    Delete commonfiles directory

--- commonfiles/cssload.php
+++
@@ -1,6 +0,0 @@
-<link rel="icon" href="../media/icon.png">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bebas Neue">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
-<link rel="stylesheet" href="../static/css/home.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
